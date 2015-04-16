a01ed6aeef97a1b35797e90c0a1acdc4b4f7d32d
MAXakaWIZARD <maxakawizard@gmail.com>
2015-04-17 00:39:58 +0300

    Add more tests

--- tests/XlsWriterTest.php
+++ tests/XlsWriterTest.php
@@ -35,7 +35,7 @@ class XlsWriterTest extends \PHPUnit_Framework_TestCase
      */
     public function testGeneral()
     {
-        $workbook = new \Xls\Writer($this->testFilePath);
+        $workbook = new Writer($this->testFilePath);
 
         //needed for test files comparison
         $workbook->setCreationTimestamp(1429042916);
@@ -63,7 +63,7 @@ class XlsWriterTest extends \PHPUnit_Framework_TestCase
      */
     public function testRich()
     {
-        $workbook = new \Xls\Writer($this->testFilePath);
+        $workbook = new Writer($this->testFilePath);
 
         //needed for test files comparison
         $workbook->setCreationTimestamp(1429042916);
@@ -74,7 +74,10 @@ class XlsWriterTest extends \PHPUnit_Framework_TestCase
         $headerFormat->setBold();
         $headerFormat->setBorder(Format::BORDER_THIN);
         $headerFormat->setColor('blue');
-        $headerFormat->setFgColor('silver');
+
+        //#ccc
+        $workbook->setCustomColor(12, 204, 204, 204);
+        $headerFormat->setFgColor(12);
 
         $cellFormat = $workbook->addFormat();
         $cellFormat->setBorder(Format::BORDER_THIN);
@@ -83,14 +86,17 @@ class XlsWriterTest extends \PHPUnit_Framework_TestCase
         $worksheet->write(0, 1, 'Count', $headerFormat);
         $worksheet->write(0, 2, 'Price', $headerFormat);
         $worksheet->write(0, 3, 'Amount', $headerFormat);
+
         $worksheet->write(1, 0, 'Intel Core i7 2600K', $cellFormat);
         $worksheet->write(1, 1, 1, $cellFormat);
         $worksheet->write(1, 2, 500, $cellFormat);
         $worksheet->writeFormula(1, 3, '=B2*C2', $cellFormat);
+
         $worksheet->write(2, 0, 'ASUS P8P67', $cellFormat);
         $worksheet->write(2, 1, 1, $cellFormat);
         $worksheet->write(2, 2, 325, $cellFormat);
         $worksheet->writeFormula(2, 3, '=B3*C3', $cellFormat);
+
         $worksheet->write(3, 0, 'DDR2-800 8Gb', $cellFormat);
         $worksheet->write(3, 1, 4, $cellFormat);
         $worksheet->write(3, 2, 100, $cellFormat);
@@ -112,10 +118,57 @@ class XlsWriterTest extends \PHPUnit_Framework_TestCase
         $worksheet->mergeCells(10, 0, 10, 2);
         $worksheet->writeFormula(10, 3, '=SUM(D2:D9)', $totalFormat);
 
+        //set some validation
+        $countValidator = $workbook->addValidator();
+        $countValidator->setPrompt('Enter valid item count');
+        $countValidator->setFormula1('INDIRECT(ADDRESS(ROW(), COLUMN())) > 0');
+        $worksheet->setValidation(1, 1, 3, 1, $countValidator);
+
         // We still need to explicitly close the workbook
         $workbook->close();
 
         $this->assertFileExists($this->testFilePath);
         $this->assertFileEquals(TEST_DATA_PATH . '/rich.xls', $this->testFilePath);
+    }
+
+    /**
+     *
+     */
+    public function testInvalidColorIndex()
+    {
+        $workbook = new Writer($this->testFilePath);
+
+        $this->setExpectedException('\Exception', 'Color index 65 outside range: 8 <= index <= 64');
+        $workbook->setCustomColor(65, 204, 204, 204);
+    }
+
+    /**
+     *
+     */
+    public function testInvalidColor()
+    {
+        $workbook = new Writer($this->testFilePath);
+
+        $this->setExpectedException('\Exception', 'Color component outside range: 0 <= color <= 255');
+        $workbook->setCustomColor(12, 265, 265, 265);
+    }
+
+    /**
+     *
+     */
+    public function testRowColToCell()
+    {
+        $this->assertSame('A1', Writer::rowcolToCell(0, 0));
+        $this->assertSame('B2', Writer::rowcolToCell(1, 1));
+        $this->assertSame('K256', Writer::rowcolToCell(255, 10));
+    }
+
+    /**
+     *
+     */
+    public function testRowColToCellInvalid()
+    {
+        $this->setExpectedException('\Exception', 'Maximum column value exceeded: 256');
+        Writer::rowcolToCell(256, 256);
     }
 }
