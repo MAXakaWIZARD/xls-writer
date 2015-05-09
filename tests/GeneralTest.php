ebd51e1da8683c1acc50d25d02f1d010b3b50378
MAXakaWIZARD <maxakawizard@gmail.com>
2015-05-09 18:31:49 +0300

    Refactor tests and add rowsizes test

--- tests/GeneralTest.php
+++ tests/GeneralTest.php
@@ -77,16 +77,12 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
     {
         $workbook = $this->createWorkbook($params);
 
-        $worksheet = $workbook->addWorksheet('My first worksheet');
+        $sheet = $workbook->addWorksheet('My first worksheet');
 
-        $worksheet->write(0, 0, 'Name');
-        $worksheet->write(0, 1, 'Age');
-        $worksheet->write(1, 0, 'John Smith');
-        $worksheet->write(1, 1, 30);
-        $worksheet->write(2, 0, 'Johann Schmidt');
-        $worksheet->write(2, 1, 31);
-        $worksheet->write(3, 0, 'Juan Herrera');
-        $worksheet->write(3, 1, 32);
+        $sheet->writeRow(0, 0, array('Name', 'Age'));
+        $sheet->writeRow(1, 0, array('John Smith', 30));
+        $sheet->writeRow(2, 0, array('Johann Schmidt', 31));
+        $sheet->writeRow(3, 0, array('Juan Herrera', 32));
 
         $workbook->save($this->testFilePath);
 
@@ -129,7 +125,7 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
     {
         $workbook = $this->createWorkbook($params);
 
-        $worksheet = $workbook->addWorksheet('New PC');
+        $sheet = $workbook->addWorksheet('New PC');
 
         $headerFormat = $workbook->addFormat();
         $headerFormat->setBold();
@@ -154,30 +150,23 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
         $priceFormat->setBorderColor('navy');
         $priceFormat->setNumFormat(2);
 
-        $worksheet->write(0, 0, 'Title', $headerFormat);
-        $worksheet->write(0, 1, 'Count', $headerFormat);
-        $worksheet->write(0, 2, 'Price', $headerFormat);
-        $worksheet->write(0, 3, 'Amount', $headerFormat);
+        $sheet->writeRow(0, 0, array('Title', 'Count', 'Price', 'Amount'), $headerFormat);
 
-        $worksheet->write(1, 0, 'Intel Core i7 2600K', $cellFormat);
-        $worksheet->write(1, 1, 1, $cellFormat);
-        $worksheet->write(1, 2, 500, $priceFormat);
-        $worksheet->writeFormula(1, 3, '=B2*C2', $priceFormat);
+        $sheet->writeRow(1, 0, array('Intel Core i7 2600K', 1), $cellFormat);
+        $sheet->write(1, 2, 500, $priceFormat);
+        $sheet->writeFormula(1, 3, '=B2*C2', $priceFormat);
 
-        $worksheet->write(2, 0, 'ASUS P8P67', $cellFormat);
-        $worksheet->write(2, 1, 1, $cellFormat);
-        $worksheet->write(2, 2, 325, $priceFormat);
-        $worksheet->writeFormula(2, 3, '=B3*C3', $priceFormat);
+        $sheet->writeRow(2, 0, array('ASUS P8P67', 1), $cellFormat);
+        $sheet->write(2, 2, 325, $priceFormat);
+        $sheet->writeFormula(2, 3, '=B3*C3', $priceFormat);
 
-        $worksheet->write(3, 0, 'DDR2-800 8Gb', $cellFormat);
-        $worksheet->write(3, 1, 4, $cellFormat);
-        $worksheet->write(3, 2, 100.15, $priceFormat);
-        $worksheet->writeFormula(3, 3, '=B4*C4', $priceFormat);
+        $sheet->writeRow(3, 0, array('DDR2-800 8Gb', 4), $cellFormat);
+        $sheet->write(3, 2, 100.15, $priceFormat);
+        $sheet->writeFormula(3, 3, '=B4*C4', $priceFormat);
 
+        $emptyRow = array_fill(0, 4, '');
         for ($i = 4; $i < 10; $i++) {
-            for ($j = 0; $j < 4; $j++) {
-                $worksheet->write($i, $j, '', $cellFormat);
-            }
+            $sheet->writeRow($i, 0, $emptyRow, $cellFormat);
         }
 
         $oldPriceFormat = $workbook->addFormat();
@@ -203,17 +192,13 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
         $grandFormat->setNumFormat(2);
         $this->assertTrue($grandFormat->isBuiltInNumFormat());
 
-        $worksheet->write(10, 0, 'Total', $grandFormat);
-        $worksheet->write(10, 1, '', $grandFormat);
-        $worksheet->write(10, 2, '', $grandFormat);
-        $worksheet->mergeCells(10, 0, 10, 2);
-        $worksheet->writeFormula(10, 3, '=SUM(D2:D10)', $oldPriceFormat);
+        $sheet->writeRow(10, 0, array('Total', '', ''), $grandFormat);
+        $sheet->mergeCells(10, 0, 10, 2);
+        $sheet->writeFormula(10, 3, '=SUM(D2:D10)', $oldPriceFormat);
 
-        $worksheet->write(11, 0, 'Grand total', $grandFormat);
-        $worksheet->write(11, 1, '', $grandFormat);
-        $worksheet->write(11, 2, '', $grandFormat);
-        $worksheet->mergeCells(11, 0, 11, 2);
-        $worksheet->writeFormula(11, 3, '=ROUND(D11-D11*0.2, 2)', $grandFormat);
+        $sheet->writeRow(11, 0, array('Grand total', '', ''), $grandFormat);
+        $sheet->mergeCells(11, 0, 11, 2);
+        $sheet->writeFormula(11, 3, '=ROUND(D11-D11*0.2, 2)', $grandFormat);
 
         //
         $discountFormat = $workbook->addFormat();
@@ -222,13 +207,13 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
         $discountFormat->setSize(14);
         $discountFormat->setFgColor('white');
         $discountFormat->setBgColor('black');
-        $worksheet->write(11, 4, '20% discount!', $discountFormat);
+        $sheet->write(11, 4, '20% discount!', $discountFormat);
 
         //set some validation
         $countValidator = $workbook->addValidator();
         $countValidator->setPrompt('Enter valid item count');
         $countValidator->setFormula1('INDIRECT(ADDRESS(ROW(), COLUMN())) > 0');
-        $worksheet->setValidation(1, 1, 3, 1, $countValidator);
+        $sheet->setValidation(1, 1, 3, 1, $countValidator);
 
         $anotherSheet = $workbook->addWorksheet('Another sheet');
         $anotherSheet->write(0, 0, 'Test');
@@ -259,9 +244,9 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
     {
         $workbook = $this->createWorkbook($params);
 
-        $worksheet = $workbook->addWorksheet();
-        $worksheet->write(0, 0, 'Test');
-        $worksheet->protect('1234');
+        $sheet = $workbook->addWorksheet();
+        $sheet->write(0, 0, 'Test');
+        $sheet->protect('1234');
 
         $workbook->save($this->testFilePath);
 
@@ -278,9 +263,9 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
     {
         $workbook = $this->createWorkbook($params);
 
-        $worksheet = $workbook->addWorksheet();
-        $worksheet->write(0, 0, 'Test');
-        $worksheet->setSelection(0, 0, 5, 5);
+        $sheet = $workbook->addWorksheet();
+        $sheet->write(0, 0, 'Test');
+        $sheet->setSelection(0, 0, 5, 5);
 
         $workbook->save($this->testFilePath);
 
@@ -313,23 +298,25 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
      * @dataProvider providerDifferentBiffVersions
      * @param array $params
      */
-    public function testDefcols($params)
+    public function testDefcolsAndRowsizes($params)
     {
         $workbook = $this->createWorkbook($params);
 
-        $worksheet = $workbook->addWorksheet();
-        $worksheet->write(0, 0, 'Test1');
-        $worksheet->write(0, 1, 'Test2');
-        $worksheet->write(0, 2, 'Test3');
+        $sheet = $workbook->addWorksheet();
+        $sheet->writeRow(0, 0, array('Test1', 'Test2', 'Test3'));
 
-        $worksheet->setColumn(0, 0, 25);
-        $worksheet->setColumn(1, 1, 50);
-        $worksheet->setColumn(2, 3, 10, null, 1);
+        $sheet->setColumn(0, 0, 25);
+        $sheet->setColumn(1, 1, 50);
+        $sheet->setColumn(2, 3, 10, null, 1);
+
+        $sheet->setRow(0, 30);
+        $sheet->setRow(1, 15);
+        $sheet->setRow(2, 10, null, 1);
 
         $workbook->save($this->testFilePath);
 
         $this->assertFileExists($this->testFilePath);
-        $correctFilePath = $this->getFilePath('defcols', $params['suffix']);
+        $correctFilePath = $this->getFilePath('defcols_rowsizes', $params['suffix']);
         $this->assertFileEquals($correctFilePath, $this->testFilePath);
     }
 
@@ -342,8 +329,8 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
         $workbook = $this->createWorkbook($params);
         $workbook->setCountry($workbook::COUNTRY_USA);
 
-        $worksheet = $workbook->addWorksheet();
-        $worksheet->write(0, 0, 'Test1');
+        $sheet = $workbook->addWorksheet();
+        $sheet->write(0, 0, 'Test1');
 
         $workbook->save($this->testFilePath);
 
