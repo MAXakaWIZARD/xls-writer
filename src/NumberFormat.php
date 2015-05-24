410a75d7100d0336940a68b012c4ed5e411bc8f6
MAXakaWIZARD <maxakawizard@gmail.com>
2015-05-25 00:02:28 +0300

    Add NumberFormat class with descriptive built-in formats constants

---
+++ src/NumberFormat.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Xls;
+
+class NumberFormat
+{
+    /**
+     * General format
+     */
+    const TYPE_GENERAL = 0;
+
+    /**
+     * Decimal: 0
+     */
+    const TYPE_DECIMAL_1 = 1;
+
+    /**
+     * Decimal: 0.00
+     */
+    const TYPE_DECIMAL_2 = 2;
+
+    /**
+     * Decimal: #,##0
+     */
+    const TYPE_DECIMAL_3 = 3;
+
+    /**
+     * Decimal: #,##0.00
+     */
+    const TYPE_DECIMAL_4 = 4;
+
+    /**
+     * Currency: "$"#,##0_);("$"#,##0)
+     */
+    const TYPE_CURRENCY_1 = 5;
+
+    /**
+     * Currency: "$"#,##0_);[Red]("$"#,##0)
+     */
+    const TYPE_CURRENCY_2 = 6;
+
+    /**
+     * Currency: "$"#,##0.00_);("$"#,##0.00)
+     */
+    const TYPE_CURRENCY_3 = 7;
+
+    /**
+     * Currency: "$"#,##0.00_);[Red]("$"#,##0.00)
+     */
+    const TYPE_CURRENCY_4 = 8;
+
+    /**
+     * Currency: _("$"* #,##0_);_("$"* (#,##0);_("$"* "-"_);_(@_)
+     */
+    const TYPE_CURRENCY_5 = 41;
+
+    /**
+     * Currency: _(* #,##0_);_(* (#,##0);_(* "-"_);_(@_)
+     */
+    const TYPE_CURRENCY_6 = 42;
+
+    /**
+     * Currency: _("$"* #,##0.00_);_("$"* (#,##0.00);_("$"* "-"??_);_(@_)
+     */
+    const TYPE_CURRENCY_7 = 43;
+
+    /**
+     * Currency: _(* #,##0.00_);_(* (#,##0.00);_(* "-"??_);_(@_)
+     */
+    const TYPE_CURRENCY_8 = 44;
+
+    /**
+     * Percent: 0%
+     */
+    const TYPE_PERCENT_1 = 9;
+
+    /**
+     * Percent: 0.00%
+     */
+    const TYPE_PERCENT_2 = 10;
+
+    /**
+     * Scientific: 0.00E+00
+     */
+    const TYPE_SCIENTIFIC_1 = 11;
+
+    /**
+     * Scientific: ##0.0E+0
+     */
+    const TYPE_SCIENTIFIC_2 = 48;
+
+    /**
+     * Fraction: # ?/?
+     */
+    const TYPE_FRACTION_1 = 12;
+
+    /**
+     * Fraction: # ??/??
+     */
+    const TYPE_FRACTION_2 = 13;
+
+    /**
+     * Date: M/D/YY
+     */
+    const TYPE_DATE_1 = 14;
+
+    /**
+     * Date: D-MMM-YY
+     */
+    const TYPE_DATE_2 = 15;
+
+    /**
+     * Date: D-MMM
+     */
+    const TYPE_DATE_3 = 16;
+
+    /**
+     * Date: MMM-YY
+     */
+    const TYPE_DATE_4 = 17;
+
+    /**
+     * Time: h:mm AM/PM
+     */
+    const TYPE_TIME_1 = 18;
+
+    /**
+     * Time: h:mm:ss AM/PM
+     */
+    const TYPE_TIME_2 = 19;
+
+    /**
+     * Time: h:mm
+     */
+    const TYPE_TIME_3 = 20;
+
+    /**
+     * Time: h:mm:ss
+     */
+    const TYPE_TIME_4 = 21;
+
+    /**
+     * Time: mm:ss
+     */
+    const TYPE_TIME_5 = 45;
+
+    /**
+     * Time: [h]:mm:ss
+     */
+    const TYPE_TIME_6 = 46;
+
+    /**
+     * Time: mm:ss.0
+     */
+    const TYPE_TIME_7 = 47;
+
+    /**
+     * Datetime: M/D/YY h:mm
+     */
+    const TYPE_DATETIME = 22;
+
+    /**
+     * Account: _(#,##0_);(#,##0)
+     */
+    const TYPE_ACCOUNT_1 = 37;
+
+    /**
+     * Account: _(#,##0_);[Red](#,##0)
+     */
+    const TYPE_ACCOUNT_2 = 38;
+
+    /**
+     * Account: _(#,##0.00_);(#,##0.00)
+     */
+    const TYPE_ACCOUNT_3 = 39;
+
+    /**
+     * Account: _(#,##0.00_);[Red](#,##0.00)
+     */
+    const TYPE_ACCOUNT_4 = 40;
+
+    /**
+     * Text: @
+     */
+    const TYPE_TEXT = 49;
+}
--- tests/RichTest.php
+++ tests/RichTest.php
@@ -2,6 +2,7 @@
 namespace Test;
 
 use Xls\Format;
+use Xls\NumberFormat;
 use Xls\Fill;
 
 /**
@@ -16,88 +17,36 @@ class RichTest extends TestAbstract
     {
         $sheet = $this->workbook->addWorksheet('New PC');
 
-        $headerFormat = $this->workbook->addFormat();
-        $headerFormat->setBold();
-        $headerFormat->setBorder(Format::BORDER_THIN);
-        $headerFormat->setBorderColor('navy');
-        $headerFormat->setColor('blue');
-        $headerFormat->setAlign('center');
-        $headerFormat->setPattern(Fill::PATTERN_GRAY50);
-
-        //#ccc
-        $this->workbook->setCustomColor(12, 204, 204, 204);
-        $headerFormat->setFgColor(12);
-
-        $cellFormat = $this->workbook->addFormat();
-        $cellFormat->setNormal();
-        $cellFormat->setBorder(Format::BORDER_THIN);
-        $cellFormat->setBorderColor('navy');
-        $cellFormat->setUnLocked();
-
-        $priceFormat = $this->workbook->addFormat();
-        $priceFormat->setBorder(Format::BORDER_THIN);
-        $priceFormat->setBorderColor('navy');
-        $priceFormat->setNumFormat(2);
-
+        $headerFormat = $this->getHeaderFormat();
         $sheet->writeRow(0, 0, array('Title', 'Count', 'Price', 'Amount'), $headerFormat);
 
-        $sheet->writeRow(1, 0, array('Intel Core i7 2600K', 1), $cellFormat);
-        $sheet->write(1, 2, 500, $priceFormat);
-        $sheet->writeFormula(1, 3, '=B2*C2', $priceFormat);
+        $cellFormat = $this->getCellFormat();
+        $countFormat = $this->getCountFormat();
+        $priceFormat = $this->getPriceFormat();
 
-        $sheet->writeRow(2, 0, array('ASUS P8P67', 1), $cellFormat);
-        $sheet->write(2, 2, 325, $priceFormat);
-        $sheet->writeFormula(2, 3, '=B3*C3', $priceFormat);
+        $partNames = array('Intel Core i7 2600K', 'ASUS P8P67', 'DDR2-800 8Gb');
+        $sheet->writeCol(1, 0, $partNames, $cellFormat);
+        $sheet->writeCol(1, 1, array(1, 1, 4), $countFormat);
+        $sheet->writeCol(1, 2, array(500, 325, 100.15), $priceFormat);
+        //should be written as formulas
+        $sheet->writeCol(1, 3, array('=B2*C2', '=B3*C3', '=B4*C4'), $priceFormat);
 
-        $sheet->writeRow(3, 0, array('DDR2-800 8Gb', 4), $cellFormat);
-        $sheet->write(3, 2, 100.15, $priceFormat);
-        $sheet->writeFormula(3, 3, '=B4*C4', $priceFormat);
-
-        $emptyRow = array_fill(0, 4, '');
-        for ($i = 4; $i < 10; $i++) {
-            $sheet->writeRow($i, 0, $emptyRow, $cellFormat);
-        }
-
-        $oldPriceFormat = $this->workbook->addFormat();
-        $oldPriceFormat->setBorder(Format::BORDER_THIN);
-        $oldPriceFormat->setBorderColor('navy');
-        $oldPriceFormat->setSize(12);
-        $oldPriceFormat->setStrikeOut();
-        $oldPriceFormat->setOutLine();
-        $oldPriceFormat->setItalic();
-        $oldPriceFormat->setShadow();
-        $oldPriceFormat->setNumFormat(2);
-        $oldPriceFormat->setLocked();
-        $oldPriceFormat->setTextWrap();
-        $oldPriceFormat->setTextRotation(0);
-
-        $grandFormat = $this->workbook->addFormat();
-        $grandFormat->setBold();
-        $grandFormat->setBorder(Format::BORDER_THIN);
-        $grandFormat->setBorderColor('navy');
-        $grandFormat->setSize(12);
-        $grandFormat->setFontFamily('Tahoma');
-        $grandFormat->setUnderline(Format::UNDERLINE_ONCE);
-        $grandFormat->setNumFormat(2);
+        $grandFormat = $this->getGrandTotalFormat();
         $this->assertTrue($grandFormat->isBuiltInNumFormat());
 
         $sheet->writeRow(10, 0, array('Total', '', ''), $grandFormat);
         $sheet->mergeCells(10, 0, 10, 2);
-        $sheet->writeFormula(10, 3, '=SUM(D2:D10)', $oldPriceFormat);
+        $sheet->writeFormula(10, 3, '=SUM(D2:D10)', $this->getOldPriceFormat());
 
         $sheet->writeRow(11, 0, array('Grand total', '', ''), $grandFormat);
         $sheet->mergeCells(11, 0, 11, 2);
         //should be written as formula
         $sheet->write(11, 3, '=ROUND(D11-D11*0.2, 2)', $grandFormat);
 
-        //
-        $discountFormat = $this->workbook->addFormat();
-        $discountFormat->setColor('red');
-        $discountFormat->setScript(Format::SCRIPT_SUPER);
-        $discountFormat->setSize(14);
-        $discountFormat->setFgColor('white');
-        $discountFormat->setBgColor('black');
-        $sheet->write(11, 4, '20% скидка!', $discountFormat);
+        $sheet->write(11, 4, '20% скидка!', $this->getDiscountFormat());
+
+        $sheet->setColumn(0, 0, 20);
+        $sheet->setColumn(3, 3, 15);
 
         $anotherSheet = $this->workbook->addWorksheet('Лист2');
         $anotherSheet->write(0, 0, 'Тест');
@@ -105,5 +54,121 @@ class RichTest extends TestAbstract
         $this->workbook->save($this->testFilePath);
 
         $this->assertTestFileEqualsTo('rich');
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getHeaderFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setBold();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setColor('blue');
+        $format->setAlign('center');
+        $format->setPattern(Fill::PATTERN_GRAY50);
+
+        //#ccc
+        $this->workbook->setCustomColor(12, 204, 204, 204);
+        $format->setFgColor(12);
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getDiscountFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setColor('red');
+        $format->setScript(Format::SCRIPT_SUPER);
+        $format->setSize(14);
+        $format->setFgColor('white');
+        $format->setBgColor('black');
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getCellFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setNormal();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setUnLocked();
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getCountFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setNormal();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setUnLocked();
+        $format->setNumFormat(NumberFormat::TYPE_DECIMAL_1);
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getGrandTotalFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setBold();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setSize(12);
+        $format->setFontFamily('Tahoma');
+        $format->setUnderline(Format::UNDERLINE_ONCE);
+        $format->setNumFormat(NumberFormat::TYPE_CURRENCY_3);
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     * @throws \Exception
+     */
+    protected function getOldPriceFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setSize(12);
+        $format->setStrikeOut();
+        $format->setOutLine();
+        $format->setItalic();
+        $format->setShadow();
+        $format->setNumFormat(NumberFormat::TYPE_CURRENCY_3);
+        $format->setLocked();
+        $format->setTextWrap();
+        $format->setTextRotation(0);
+
+        return $format;
+    }
+
+    /**
+     * @return Format
+     */
+    protected function getPriceFormat()
+    {
+        $format = $this->workbook->addFormat();
+        $format->setBorder(Format::BORDER_THIN);
+        $format->setBorderColor('navy');
+        $format->setNumFormat(NumberFormat::TYPE_CURRENCY_3);
+
+        return $format;
     }
 }
